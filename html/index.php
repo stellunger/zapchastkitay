1cda0454e84515d9f83264d1a9dfd1d8d88dfa7e
stellunger <1@stellunger.ru>
2013-10-16 19:18:29 +0600

    final

--- html/index.php
+++ html/index.php
@@ -73,7 +73,7 @@
 
       <div class="row">
         <div class="col-sm-12">        
-        <h2 align=center>Звоните бесплатно: 8 800 555-39-84.</h2>
+        <h2 align=center>Звонок по России бесплатный<br/>8 800 555-39-84</h2>
         </div>
       </div>
       
@@ -133,7 +133,7 @@
       <div class="row">
         <div class="col-sm-12">
         <h2 align=center>И многое другое!</h2>
-        <h2 align=center>Звоните бесплатно: 8 800 555-39-84.</h2>
+        <h2 align=center>Звонок по России бесплатный<br/>8 800 555-39-84</h2>
         </div>
       </div>
 
@@ -153,5 +153,17 @@
     </div> 
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
+    <!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter22607902 = new Ya.Metrika({id:22607902, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/22607902" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-44121958-4']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </body>
 </html>
